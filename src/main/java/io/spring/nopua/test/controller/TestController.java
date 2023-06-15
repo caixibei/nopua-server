b91c9b0ead6feb72738cd006db707feb51e7d9a4
caixibei@139.com <caixibei@139.com>
2023-06-15 09:58:19 +0800

    删除无用导入

--- src/main/java/io/spring/nopua/test/controller/TestController.java
+++ src/main/java/io/spring/nopua/test/controller/TestController.java
@@ -2,7 +2,6 @@ package io.spring.nopua.test.controller;
 import io.spring.nopua.test.pojo.Test;
 import io.spring.nopua.test.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
