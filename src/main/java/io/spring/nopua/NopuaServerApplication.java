41e64b4993d94de22bf816b5cc17b1035c9a5df5
caixibei@139.com <caixibei@139.com>
2023-06-20 13:59:26 +0800

    简化启动类

--- src/main/java/io/spring/nopua/NopuaServerApplication.java
+++ src/main/java/io/spring/nopua/NopuaServerApplication.java
@@ -1,19 +1,13 @@
 package io.spring.nopua;
-import io.swagger.v3.oas.models.OpenAPI;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.core.env.Environment;
 @SpringBootApplication(scanBasePackages = {"io.spring.nopua"})
 @MapperScan(value = {"io.spring.nopua.*.mapper"})
 @Slf4j
 public class NopuaServerApplication {
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(NopuaServerApplication.class, args);
-        Environment environment = context.getEnvironment();
-        String port = environment.getProperty("server.port");
+        SpringApplication.run(NopuaServerApplication.class, args);
     }
-
 }
