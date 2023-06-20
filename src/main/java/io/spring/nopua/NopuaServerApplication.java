ce6277418771a9456bbd851da934329e9046ac5b
caixibei@139.com <caixibei@139.com>
2023-06-20 14:15:21 +0800

    启动类去除日志注解

--- src/main/java/io/spring/nopua/NopuaServerApplication.java
+++ src/main/java/io/spring/nopua/NopuaServerApplication.java
@@ -1,11 +1,9 @@
 package io.spring.nopua;
-import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = {"io.spring.nopua"})
 @MapperScan(value = {"io.spring.nopua.*.mapper"})
-@Slf4j
 public class NopuaServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(NopuaServerApplication.class,args);
